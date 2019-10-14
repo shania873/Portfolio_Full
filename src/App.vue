2ea1e971e5151907f330bc50b7f6df1b523d4daf
Caroline van Aerschot <vanaeca@hotmail.com>
2019-10-15 01:07:20 +0200

    commit derniers changements pour le nettoyage

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,23 @@
 <template>
   <div id="app">
     <div id="page">
-      <div id="nav_bar">
-        <a class="logo" rel="index" href="https://carolinevanaerschot.be">
-          <img src="../src/assets/img/logoCaro.svg" alt="logo" />
-        </a>
-        <nav id="navbar">
-          <router-link to="/" class="home-link">
-            <i class="fas fa-home"></i>
-          </router-link>
-          <router-link to="/about" class>
-            <i class="fas fa-user"></i>
-          </router-link>
-          <router-link to="/portfolio" class>
-            <i class="far fa-eye"></i>
-          </router-link>
-          <router-link to="/contact" class>
-            <i class="far fa-address-book"></i>
-          </router-link>
-        </nav>
-        <ul>
-          <li>
-            <a href="https://github.com/shania873/Portfolio_Full" target="_blank">
-              <i class="fab fa-github"></i>
-            </a>
-          </li>
-          <li>
-            <a href="https://www.linkedin.com/in/caroline-v-65b750113/" target="_blank">
-              <i class="fab fa-linkedin"></i>
-            </a>
-          </li>
-        </ul>
-        <a id="mobile-link" class="burgerMenu" onclick="myFunction()">
-          <i class="fa fa-bars"></i>
-        </a>
-      </div>
+           <SiteHeader></SiteHeader>      
       <template>
               <transition name="fade" mode="out-in">
                   <router-view />
               </transition>
       </template>
-
     </div>
   </div>
 </template>
 <script>
 
-
+import SiteHeader from '../src/components/header.vue';
 
 export default {
+ components: {
+  	SiteHeader
+  },
   mounted: function() {
     (function() {
       var element;
---
+++ src/components/header.vue
@@ -0,0 +1,56 @@
+<template>
+   <header>
+      <div id="nav_bar">
+        <a class="logo" rel="index" href="https://carolinevanaerschot.be">
+          <img src="../assets/img/logoCaro.svg" alt="logo" />
+        </a>
+        <nav id="navbar">
+          <router-link to="/" class="home-link">
+            <i class="fas fa-home"></i>
+          </router-link>
+          <router-link to="/about" class>
+            <i class="fas fa-user"></i>
+          </router-link>
+          <router-link to="/portfolio" class>
+            <i class="far fa-eye"></i>
+          </router-link>
+          <router-link to="/contact" class>
+            <i class="far fa-address-book"></i>
+          </router-link>
+        </nav>
+        <ul>
+          <li>
+            <a href="https://github.com/shania873/Portfolio_Full" target="_blank">
+              <i class="fab fa-github"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/caroline-v-65b750113/" target="_blank">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </li>
+        </ul>
+        <a id="mobile-link" class="burgerMenu" onclick="myFunction()">
+          <i class="fa fa-bars"></i>
+        </a>
+      </div>
+   </header>
+
+</template>
+
+<script>
+export default {
+    mounted: function(){
+(function() {
+      var element;
+      function myFunction() {
+        element = document.getElementById("navbar");
+        element.classList.toggle("open");
+      }
+      document.getElementById("mobile-link").onclick = function() {
+        myFunction();
+       };
+    })();
+    }
+}
+</script>
